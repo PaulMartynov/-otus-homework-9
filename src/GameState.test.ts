9c0089e80b39c8bfbb53a2d29a4ae90d65fe1f4d
Pavel Martynov <p.martynov@antraks.ru>
2021-08-08 22:33:16 +0300

    add gameState functions

--- src/GameState.test.ts
+++ src/GameState.test.ts
@@ -15,3 +15,92 @@ describe("testing isAnyoneAlive function", () => {
     expect(GameState.isAnyoneAlive(str)).toBeTruthy();
   });
 });
+
+describe("testing getFieldMatrixTest", () => {
+  test("is a function", () => {
+    expect(GameState.getFieldMatrix).toBeInstanceOf(Function);
+  });
+  test("checking returning value", () => {
+    const table = document.createElement("table");
+    table.innerHTML =
+      "<tr><td></td><td></td><td></td></tr><tr><td></td><td></td></tr>";
+    const fieldMatrix = GameState.getFieldMatrix(table);
+    expect(fieldMatrix).toBeInstanceOf(Array);
+    expect(fieldMatrix).toHaveLength(2);
+    expect(fieldMatrix[0]).toHaveLength(3);
+  });
+});
+
+describe("testing checkCellState function", () => {
+  const table = document.createElement("table");
+  table.innerHTML =
+    "<tr><td class='dead'></td><td class='alive'></td><td class='doomed'></td></tr><tr><td></td><td></td></tr>";
+  const fieldMatrix = GameState.getFieldMatrix(table);
+  test("is a function", () => {
+    expect(GameState.checkCellState).toBeInstanceOf(Function);
+  });
+  test("must return 0 when row is not exist", () => {
+    expect(GameState.checkCellState(fieldMatrix, 3, 3)).toBe(0);
+  });
+  test("must return 0 when cell is not exist", () => {
+    expect(GameState.checkCellState(fieldMatrix, 3, 1)).toBe(0);
+  });
+  test("must return 0 when cell is dead", () => {
+    expect(GameState.checkCellState(fieldMatrix, 0, 0)).toBe(0);
+  });
+  test("must return 1 when cell is alive", () => {
+    expect(GameState.checkCellState(fieldMatrix, 1, 0)).toBe(1);
+  });
+  test("must return 1 when cell is doomed", () => {
+    expect(GameState.checkCellState(fieldMatrix, 2, 0)).toBe(1);
+  });
+});
+
+describe("testing chekNeighbours function", () => {
+  const table = document.createElement("table");
+  table.innerHTML =
+    "<tr><td class='dead'></td><td class='alive'></td><td class='doomed'></td></tr>" +
+    "<tr><td class='alive'></td><td class='dead'></td><td class='alive'></td></tr>" +
+    "<tr><td class='dead'></td><td class='dead'></td><td class='alive'></td></tr>" +
+    "<tr><td class='dead'></td><td class='dead'></td><td class='dead'></td></tr>";
+  const fieldMatrix = GameState.getFieldMatrix(table);
+  test("is a function", () => {
+    expect(GameState.chekNeighbours).toBeInstanceOf(Function);
+  });
+  [
+    [0, 0, 2],
+    [1, 0, 3],
+    [0, 3, 0],
+    [1, 3, 1],
+  ].forEach(([x, y, result]) => {
+    test(`for x='${x}', y='${y}' must return '${result}'`, () => {
+      expect(GameState.chekNeighbours(fieldMatrix, x, y)).toBe(result);
+    });
+  });
+});
+
+describe("testing updateCellState function", () => {
+  const table = document.createElement("table");
+  table.innerHTML =
+    "<tr><td class='alive'></td><td class='alive'></td><td class='doomed'></td></tr>" +
+    "<tr><td class='alive'></td><td class='alive'></td><td class='alive'></td></tr>" +
+    "<tr><td class='dead'></td><td class='dead'></td><td class='alive'></td></tr>" +
+    "<tr><td class='dead'></td><td class='dead'></td><td class='dead'></td></tr>";
+  const fieldMatrix = GameState.getFieldMatrix(table);
+  test("is a function", () => {
+    expect(GameState.updateCellState).toBeInstanceOf(Function);
+  });
+  [
+    [1, 0, "dead"],
+    [1, 2, "dead"],
+    [2, 2, "alive"],
+    [1, 3, "dead"],
+  ].forEach(([x, y, result]) => {
+    test(`for x='${x}', y='${y}' cell must be '${result}'`, () => {
+      GameState.updateCellState(fieldMatrix, Number(x), Number(y));
+      expect(fieldMatrix[Number(y)][Number(x)].getAttribute("class")).toBe(
+        result
+      );
+    });
+  });
+});
--- src/GameState.ts
+++ src/GameState.ts
@@ -1,3 +1,73 @@
-export function isAnyoneAlive(field: Element): boolean {
-  return field.querySelectorAll(".alive").length > 0;
+import * as Field from "./Field";
+
+export function isAnyoneAlive(webEl: Element): boolean {
+  return webEl.querySelectorAll(".alive").length > 0;
+}
+
+export function getFieldMatrix(webEl: Element): Array<NodeListOf<Element>> {
+  const result = [];
+  const strings = webEl.querySelectorAll("tr");
+  for (let i = 0; i < strings.length; i += 1) {
+    result.push(strings[i].querySelectorAll("td"));
+  }
+  return result;
+}
+
+export function checkCellState(
+  field: Array<NodeListOf<Element>>,
+  x: number,
+  y: number
+): number {
+  const row = field[y];
+  if (row === undefined) {
+    return 0;
+  }
+  const cell = row[x];
+  if (cell === undefined) {
+    return 0;
+  }
+  if (
+    cell.getAttribute("class") === "alive" ||
+    cell.getAttribute("class") === "doomed"
+  ) {
+    return 1;
+  }
+  return 0;
+}
+
+export function chekNeighbours(
+  field: Array<NodeListOf<Element>>,
+  x: number,
+  y: number
+): number {
+  let countOfAlive = 0;
+  for (let i = x - 1; i <= x + 1; i += 1) {
+    countOfAlive += checkCellState(field, i, y - 1);
+  }
+  for (let i = x - 1; i <= x + 1; i += 1) {
+    countOfAlive += checkCellState(field, i, y + 1);
+  }
+  countOfAlive += checkCellState(field, x - 1, y);
+  countOfAlive += checkCellState(field, x + 1, y);
+  return countOfAlive;
+}
+
+export function updateCellState(
+  field: Array<NodeListOf<Element>>,
+  x: number,
+  y: number
+): void {
+  const countOfNeighbours = chekNeighbours(field, x, y);
+  if (countOfNeighbours === 3) {
+    Field.setAlive(field[y][x]);
+  }
+  if (countOfNeighbours > 3 || countOfNeighbours < 2) {
+    Field.setDead(field[y][x]);
+  }
+  if (
+    countOfNeighbours === 2 &&
+    field[y][x].getAttribute("class") === "alive"
+  ) {
+    Field.setAlive(field[y][x]);
+  }
 }
